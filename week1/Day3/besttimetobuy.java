c4c2303b82d65bbe6d65f34fd483018f9bdab861
karthikhaa14 <115576385+karthikhaa14@users.noreply.github.com>
2023-08-20 17:13:06 +0530

    Create besttimetobuy.java

---
+++ week1/Day3/besttimetobuy.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min=prices[0];
+        int max=0;
+        for(int i=1;i<prices.length;i++){
+            if(prices[i]<min){
+                min=prices[i];
+            }   
+            if(max<prices[i]-min){
+                max=prices[i]-min;
+            }
+        }
+        return max;
+        
+       
+    }
+}
