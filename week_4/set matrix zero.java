f3cc0a8cf412c57c8aa0a3cabb4c8aa0b8b01121
karthikhaa14 <115576385+karthikhaa14@users.noreply.github.com>
2023-09-03 19:36:34 +0530

    day_22

---
+++ week_4/set matrix zero.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int row=matrix.length,col=matrix[0].length;
+        int rowarr[]=new int[row];
+        int colarr[]=new int[col];
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(matrix[i][j]==0){
+                    rowarr[i]=1;
+                    colarr[j]=1;
+                }
+            }
+        }
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(rowarr[i]==1||colarr[j]==1)
+                    matrix[i][j]=0;
+            }
+        }
+    }
+}
